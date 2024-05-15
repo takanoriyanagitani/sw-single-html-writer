ec899ddfb5281cf89baf8772841d7ef420079968
yanagitani <takanori.yanagitani@gmail.com>
2024-05-16 07:31:05 +0900

    docs: sample app to print an img tag

--- Sources/SampleApp/App.swift
+++ Sources/SampleApp/App.swift
@@ -3,3 +3,7 @@ import SingleHtmlWriter
 let jsPath: String = "./sample.d/index.js"
 let scTag: String = SingleHtmlWriter.jspath2scriptDefault(jsPath)
 print(scTag)
+
+let pngPath: String = "./sample.d/img.png"
+let imgTag: String = SingleHtmlWriter.pngpath2imgDefault(pngPath)
+print(imgTag)
